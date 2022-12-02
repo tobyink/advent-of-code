798aa19ae70c77b6ec1a9fa08d8eb4e98aeb58da
Toby Inkster <github@toby.ink>
2022-12-02 10:32:26 +0000

    Rust implementation for day 1

---
+++ day-1/day1.rs
@@ -0,0 +1,43 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+pub fn main() {
+    let file = File::open("input.txt").unwrap();
+    let io = BufReader::new(file);
+
+    // This could probably be made a lot more concise...
+    let mut elf_no: i32 = 0;
+    let mut calories = HashMap::<i32, isize>::new();
+    for line in io.lines() {
+        match line {
+            Ok(line) => match &line[..] {
+                "" => elf_no += 1,
+                n => {
+                    let i = n.parse::<isize>().unwrap();
+                    match calories.get(&elf_no) {
+                        Some(j) => calories.insert(elf_no, i + j),
+                        None => calories.insert(elf_no, i),
+                    };
+                }
+            },
+            Err(_) => panic!("bad line read"),
+        }
+    }
+
+    let mut sorted: Vec<(i32, isize)> = calories
+        .keys()
+        .map(|k| (*k, calories[k]))
+        .collect::<Vec<(i32, isize)>>();
+    sorted.sort_by_key(|i| i.1);
+    sorted.reverse();
+
+    println!( "Elf {} has {} calories.", sorted[0].0, sorted[0].1 );
+
+    let mut total: isize = 0;
+    for n in 0..=2 {
+        total += sorted[n].1;
+    }
+    println!( "The top three elves have {} calories total.", total );
+}
+
