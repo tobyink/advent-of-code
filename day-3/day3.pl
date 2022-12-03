c2c6c07d6b7de577aa179836d226ec05dd73a5f4
Toby Inkster <github@toby.ink>
2022-12-03 14:30:07 +0000

    Day 3 in Perl

---
+++ day-3/day3.pl
@@ -0,0 +1,38 @@
+#!perl
+
+use v5.16;
+use warnings;
+
+my $i = 0;
+my %priorities = map { $_ => ++$i } 'a'..'z', 'A'..'Z';
+
+PART1: {
+	local @ARGV = 'input.txt';
+	my $total = 0;
+	while ( <> ) {
+		chomp;
+		my @parts = ( substr( $_, 0, length()/2 ), substr( $_, length()/2 ) );
+		my $re = qr/[^$parts[1]]/;
+		$total += $priorities{ substr( $parts[0] =~ s/$re//gr, 0, 1 ) };
+	}
+	say "Total: $total";
+}
+
+PART2: {
+	local @ARGV = 'input.txt';
+	my $total = 0;
+	while () {
+		chomp( my $e1 = <> // last );
+		chomp( my $e2 = <> );
+		chomp( my $e3 = <> );
+		for my $letter ( 'a'..'z', 'A'..'Z' ) {
+			next unless (
+				index( $e1, $letter ) >= 0 and
+				index( $e2, $letter ) >= 0 and
+				index( $e3, $letter ) >= 0
+			);
+			$total += $priorities{$letter};
+		}
+	}
+	say "Total: $total";
+}
