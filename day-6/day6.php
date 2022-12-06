9351d556bb04243ccec7fe704b1b3b6fce8e94f0
Toby Inkster <github@toby.ink>
2022-12-06 10:42:51 +0000

    Day 6 implementation in PHP

---
+++ day-6/day6.php
@@ -0,0 +1,20 @@
+<?php
+
+function find_marker ( &$buffer, $marker_size ) {
+	$length = strlen( $buffer );
+	for ( $pos = $marker_size; $pos <= $length; $pos++ ) {
+		$chars = array_unique(
+			str_split(
+				substr( $buffer, $pos - $marker_size, $marker_size )
+			)
+		);
+		if ( count($chars) == $marker_size )
+			return $pos;
+	}
+	return 0;
+}
+
+$input = file_get_contents( "input.txt" );
+
+echo 'Start of packet:  ' . find_marker( $input, 4 ) . "\n";
+echo 'Start of message: ' . find_marker( $input, 14 ) . "\n";
