a838609bd0a145dcd0b1c7c16a6636bdcc36bd64
Toby Inkster <github@toby.ink>
2022-12-07 18:26:13 +0000

    Second pass at Perl version of Day 6

---
+++ day-6/day6-bis.pl
@@ -0,0 +1,14 @@
+#!perl
+use strict; use warnings; use experimental qw( signatures );
+
+sub find_marker ( $buffer, $marker_size ) {
+	for my $pos ( $marker_size .. length $buffer ) {
+		my %chars = map { substr( $buffer, $pos-$_, 1 ) => 1 } 1 .. $marker_size;
+		return $pos if keys( %chars ) == $marker_size;
+	}
+	return 0;
+}
+
+my $input = do { ( @ARGV, $/ ) = 'input.txt'; <> };
+printf "Start of packet:  %d\n", find_marker( $input, 4 );
+printf "Start of message: %d\n", find_marker( $input, 14 );
