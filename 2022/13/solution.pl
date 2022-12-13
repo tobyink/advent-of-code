d78a2b813abb6c2bb025cf5a73c12b09a730dd22
Toby Inkster <github@toby.ink>
2022-12-13 12:07:38 +0000

    Replace my_cmp with packet_cmp which is better named, returns the opposite sign, and has a prototype making it play nice with sort.

--- 2022/13/solution.pl
+++ 2022/13/solution.pl
@@ -3,13 +3,13 @@ use v5.16;
 use List::Util qw( product );
 use constant FILENAME => 'input.txt';
 
-sub my_cmp {
-	return $_[1] <=> $_[0] unless grep ref, @_;
+sub packet_cmp ($$) {
+	return $_[0] <=> $_[1] unless grep ref, @_;
 	my ( $ix, $cmp, $a, $b ) = ( -1, 0, map ref ? $_ : [$_], @_ );
 	while ( defined $ix++ ) {
-		return $ix <= $#$b if $ix > $#$a;
-		return -1          if $ix > $#$b;
-		return $cmp        if $cmp = my_cmp( $a->[$ix], $b->[$ix] );
+		return $ix <= $#$a if $ix > $#$b;
+		return -1          if $ix > $#$a;
+		return $cmp        if $cmp = packet_cmp( $a->[$ix], $b->[$ix] );
 	}
 }
 
@@ -18,7 +18,7 @@ PART_1: {
 	my ( $count, $total ) = ( 0, 0 );
 	while ( ++$count and not eof $fh ) {
 		my ( $a, $b ) = map eval scalar <$fh>, 1..3;
-		$total += $count unless my_cmp( $a, $b ) < 0;
+		$total += $count unless packet_cmp( $a, $b ) > 0;
 	}
 	say "Index total: $total";
 }
@@ -26,7 +26,7 @@ PART_1: {
 PART_2: {
 	open my $fh, '<', FILENAME or die;
 	my @markers = ( [[2]], [[6]] );
-	my @all = sort { my_cmp( $b, $a ) } @markers, map eval, <$fh>;
+	my @all = sort packet_cmp @markers, map eval, <$fh>;
 	say "Decoder key: ", product grep {
 		$all[$_-1]==$markers[0] or $all[$_-1]==$markers[1]
 	} 1 .. @all;
