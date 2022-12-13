790ecbc642dd9264edea4f10d4968108c6fb2687
Toby Inkster <github@toby.ink>
2022-12-13 10:24:58 +0000

    Perl solution for day 13; pretty easy

---
+++ 2022/13/solution.pl
@@ -0,0 +1,46 @@
+#!perl
+use v5.16;
+use warnings;
+use JSON::PP qw( encode_json decode_json );
+
+sub my_cmp {
+	my ( $x, $y ) = @_;
+	return $y <=> $x          if ( !ref $x and !ref $y );
+	return my_cmp( [$x], $y ) if ( !ref $x and  ref $y );
+	return my_cmp( $x, [$y] ) if (  ref $x and !ref $y );
+	my $i = 0;
+	while () {
+		return  0 if ( $i > $#$x and $i > $#$y );
+		return  1 if ( $i > $#$x );
+		return -1 if ( $i > $#$y );
+		my $cmp = my_cmp( $x->[$i], $y->[$i] );
+		return $cmp if $cmp;
+		++$i;
+	}
+}
+
+my $filename = 'input.txt';
+
+PART_1: {
+	my ( $i, $total ) = ( 0, 0 );
+	open( my $fh, '<', $filename );
+	while ( ++$i and not eof $fh ) {
+		my $first  = decode_json( scalar <$fh> );
+		my $second = decode_json( scalar <$fh> );
+		my $blank  = scalar <$fh>;
+		$total += $i unless my_cmp( $first, $second ) < 0;
+	}
+	say "Index total: $total";
+}
+
+PART_2: {
+	open( my $fh, '<', $filename );
+	my @all = map { chomp; length($_) ? decode_json($_) : () } <$fh>;
+	my @markers = ( [[2]], [[6]] );
+	push @all, @markers;
+	my @sorted = sort { my_cmp($b, $a) } @all;
+	my @indices = grep {
+		$sorted[$_-1]==$markers[0] or $sorted[$_-1]==$markers[1]
+	} 1 .. @sorted;
+	say "Decoder key: ", $indices[0] * $indices[1];
+}
