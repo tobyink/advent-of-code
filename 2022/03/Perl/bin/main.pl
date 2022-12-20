7e64a62c1599ed7575e98174d7267ccc65c1b2a8
Toby Inkster <github@toby.ink>
2022-12-20 14:39:05 +0000

    Port day 3 to new harness

--- 2022/03/Perl/bin/main.pl
+++ 2022/03/Perl/bin/main.pl
@@ -2,12 +2,13 @@
 
 use v5.16;
 use warnings;
+use constant FILENAME => $ENV{ADVENT_INPUT};
 
 my $i = 0;
 my %priorities = map { $_ => ++$i } 'a'..'z', 'A'..'Z';
 
 PART1: {
-	local @ARGV = 'input.txt';
+	local @ARGV = FILENAME;
 	my $total = 0;
 	while ( <> ) {
 		chomp;
@@ -15,11 +16,11 @@ PART1: {
 		my $re = qr/[^$parts[1]]/;
 		$total += $priorities{ substr( $parts[0] =~ s/$re//gr, 0, 1 ) };
 	}
-	say "Total: $total";
+	say "PART1: $total";
 }
 
 PART2: {
-	local @ARGV = 'input.txt';
+	local @ARGV = FILENAME;
 	my $total = 0;
 	while () {
 		chomp( my $e1 = <> // last );
@@ -34,5 +35,5 @@ PART2: {
 			$total += $priorities{$letter};
 		}
 	}
-	say "Total: $total";
+	say "PART2: $total";
 }
--- 2022/03/Ruby/main.rb
+++ 2022/03/Ruby/main.rb
@@ -5,16 +5,16 @@ i = 0
 priorities = letters.map { |letter| i += 1; [ letter, i ] }.to_h
 
 total = 0
-File.readlines( 'input.txt' ).each do |line|
+File.readlines( ENV["ADVENT_INPUT"] ).each do |line|
 	line = line.chomp
 	first, second = line.chars.each_slice( line.length / 2 ).map( &:join )
 	match = first.match /([#{second}])/
 	total += priorities[match[1]]
 end
-puts( "Total: %s" % [ total ] );
+puts( "PART1: %s" % [ total ] );
 
 total = 0
-File.readlines( 'input.txt' ).each_slice(3) do |line|
+File.readlines( ENV["ADVENT_INPUT"] ).each_slice(3) do |line|
 	letters.each do |letter|
 		if line[0].include? letter and
 			line[1].include? letter and
@@ -23,4 +23,4 @@ File.readlines( 'input.txt' ).each_slice(3) do |line|
 		end
 	end
 end
-puts( "Total: %s" % [ total ] );
+puts( "PART2: %s" % [ total ] );
